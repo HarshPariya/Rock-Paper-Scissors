6a84ab2cd6caae5b6a9a9bebec0b97ce540938d8
Harsh-Pariya <harshpariya195@gmail.com>
2025-03-28 18:16:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Play from "./components/Play";
 import Game from "./components/Game";
 import Footer from "./components/Footer";
 import { Routes, Route } from "react-router-dom";
+import axios from "axios";
 
 function App() {
   const [myChoice, setMyChoice] = useState("");
   const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/score").then((res) => setScore(res.data));
+  }, []);
+
+  useEffect(() => {
+    axios.post("http://localhost:5000/api/score", { value: score });
+  }, [score]);
 
   return (
     <>
@@ -15,12 +24,7 @@ function App() {
         <Header score={score} />
         <Routes>
           <Route path="/" element={<Play setMyChoice={setMyChoice} />} />
-          <Route
-            path="/game"
-            element={
-              <Game myChoice={myChoice} score={score} setScore={setScore} />
-            }
-          />
+          <Route path="/game" element={<Game myChoice={myChoice} score={score} setScore={setScore} />} />
         </Routes>
       </div>
       <Footer />
